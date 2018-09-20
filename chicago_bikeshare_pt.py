00d9d3aed7ad7a1309f7350add40d3b0c63d449b
Anderson <andersonpds14@gmail.com>
2018-09-20 11:42:07 -0300

    Pequenas alterações no código

--- chicago_bikeshare_pt.py
+++ chicago_bikeshare_pt.py
@@ -28,6 +28,8 @@ input("Aperte Enter para continuar...")
 # TAREFA 1
 # TODO: Imprima as primeiras 20 linhas usando um loop para identificar os dados.
 print("\n\nTAREFA 1: Imprimindo as primeiras 20 amostras")
+for i in range(20):
+    print(data_list[i])
 
 # Vamos mudar o data_list para remover o cabeçalho dele.
 data_list = data_list[1:]
@@ -38,9 +40,11 @@ data_list = data_list[1:]
 input("Aperte Enter para continuar...")
 # TAREFA 2
 # TODO: Imprima o `gênero` das primeiras 20 linhas
-
 print("\nTAREFA 2: Imprimindo o gênero das primeiras 20 amostras")
-
+for data in data_list:
+    for i in range(20):
+        print(data[6])
+    break
 
 # Ótimo! Nós podemos pegar as linhas(samples) iterando com um for, e as colunas(features) por índices.
 # Mas ainda é difícil pegar uma coluna em uma lista. Exemplo: Lista com todos os gêneros
